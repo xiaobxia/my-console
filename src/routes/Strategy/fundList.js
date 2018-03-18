e3da916416d7cb0671e8ffdf57e92e138aa93de0
xiaobxia <673806687@qq.com>
2018-03-18 09:43:28 +0800

    xg

--- src/routes/Strategy/fundList.js
+++ src/routes/Strategy/fundList.js
@@ -27,13 +27,14 @@ class FundList extends PureComponent {
           const rule = record.rule;
           return (
             <p>
-              {rule.indexOf('isSlump') !== -1 && <Tag color="green">近期暴跌</Tag>}
-              {rule.indexOf('isSupport') !== -1 && <Tag color="magenta">支撑</Tag>}
-              {rule.indexOf('isLow') !== -1 && <Tag color="purple">低位</Tag>}
-              {rule.indexOf('isLowHalf') !== -1 && <Tag color="purple">半年低位</Tag>}
-              {rule.indexOf('internal') !== -1 && <Tag color="gold">连续性</Tag>}
-              {rule.indexOf('isMin') !== -1 && <Tag color="lime">新低</Tag>}
-              {rule.indexOf('distribution') !== -1 && <Tag color="orange">幅度</Tag>}
+              {rule.indexOf('isSlump') !== -1 && <Tag color="#52c41a">近期暴跌</Tag>}
+              {rule.indexOf('isWeekSlump') !== -1 && <Tag color="#b7eb8f">超短暴跌</Tag>}
+              {rule.indexOf('isSupport') !== -1 && <Tag color="#f50">支撑</Tag>}
+              {rule.indexOf('isLow') !== -1 && <Tag color="#722ed1">低位</Tag>}
+              {rule.indexOf('isLowHalf') !== -1 && <Tag color="#d3adf7">半年低位</Tag>}
+              {rule.indexOf('internal') !== -1 && <Tag color="#13c2c2">连续性</Tag>}
+              {rule.indexOf('isMin') !== -1 && <Tag color="#108ee9">新低</Tag>}
+              {rule.indexOf('distribution') !== -1 && <Tag color="#f50">幅度</Tag>}
             </p>
           );
         }
@@ -44,9 +45,11 @@ class FundList extends PureComponent {
           const rule = record.saleRule;
           return (
             <p>
-              {rule.indexOf('isBoom') !== -1 && <Tag color="#f50">近期暴涨</Tag>}
-              {rule.indexOf('isHigh') !== -1 && <Tag color="#108ee9">高位</Tag>}
-              {rule.indexOf('isHighHalf') !== -1 && <Tag color="#108ee9">半年高位</Tag>}
+              {rule.indexOf('isBoom') !== -1 && <Tag color="red">近期暴涨</Tag>}
+              {rule.indexOf('isWeekSlump') !== -1 && <Tag color="magenta">超短暴涨</Tag>}
+              {rule.indexOf('isHigh') !== -1 && <Tag color="gold">高位</Tag>}
+              {rule.indexOf('isHighHalf') !== -1 && <Tag color="orange">半年高位</Tag>}
+              {rule.indexOf('downInternal') !== -1 && <Tag color="cyan">连续性</Tag>}
             </p>
           );
         }
