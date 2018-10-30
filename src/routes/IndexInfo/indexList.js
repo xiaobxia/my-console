d42335d3bb60b875fcef33688c195c5ec17e6d35
xiaobxia <673806687@qq.com>
2018-10-30 17:17:00 +0800

    xg

--- src/routes/IndexInfo/indexList.js
+++ src/routes/IndexInfo/indexList.js
@@ -12,8 +12,8 @@ import {CopyToClipboard} from 'react-copy-to-clipboard';
 const fnMap = indexInfoUtil.fnMap;
 const InfoUtil = indexInfoUtil.Util;
 
-const functionName = 'ifBuyYiliao'
-const hide = 'sell'
+const functionName = 'ifSellZhengquan'
+const hide = 'buy'
 
 class IndexList extends PureComponent {
 
