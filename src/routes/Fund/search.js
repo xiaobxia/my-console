839214102b5005480dd86916331075033ff9e26f
xiaobxia <673806687@qq.com>
2018-11-07 10:49:07 +0800

    xg

--- src/routes/Fund/search.js
+++ src/routes/Fund/search.js
@@ -2,9 +2,10 @@
  * Created by xiaobxia on 2018/2/12.
  */
 import React, {PureComponent} from 'react'
-import {Modal, Form, Input, Button, Spin} from 'antd';
+import {Modal, Form, Input, Button, Spin, Select} from 'antd';
 
 const FormItem = Form.Item;
+const Option = Select.Option;
 
 class Search extends PureComponent {
   handleOk = (e) => {
@@ -22,6 +23,13 @@ class Search extends PureComponent {
       let data = {
         ...getFieldsValue()
       };
+      if (data.sell === 'all') {
+        delete data.sell
+      } else if (data.sell === 'sell') {
+        data.sell = true
+      } else if (data.sell === 'noSell') {
+        data.sell = false
+      }
       onSearch(data)
     });
   };
@@ -35,6 +43,15 @@ class Search extends PureComponent {
     return (
       <div>
         <Form layout="inline" style={{textAlign: 'center'}}>
+          <FormItem label="销售状态">
+            {getFieldDecorator('sell')(
+              <Select defaultValue="all" style={{ width: 120 }}>
+                <Option value="all">全部</Option>
+                <Option value="sell">可售</Option>
+                <Option value="noSell">不可售</Option>
+              </Select>
+            )}
+          </FormItem>
           <FormItem label="关键词">
             {getFieldDecorator('keyword')(
               <Input.Search
